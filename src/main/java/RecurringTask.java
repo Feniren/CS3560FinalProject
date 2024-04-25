a00db3d3e4c0f858adb2416ffe0bec5d978e02cc
Feniren <112586282+Feniren@users.noreply.github.com>
2024-04-25 16:40:41 -0700

    Add files via upload

---
+++ src/main/java/RecurringTask.java
@@ -0,0 +1,7 @@
+import java.time;
+
+public class RecurringTask extends task{
+  LocalDate StartDate;
+  LocalDate EndDate;
+  //enum for frequency
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+public class Task{
+  String Name;
+  String Type;
+  int StartTime;
+  int Duration;
+  
+  Task(){
+    Name = "Your mom";
+    Type = "Idk";
+  }
+
+  SetType(String Type){
+  }
+}
