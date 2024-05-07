24b104ae36fe5e7a9e804bdc9f4badcf89f76781
Feniren <sanders.aidan.c.ss@gmail.com>
2024-05-07 16:02:20 -0700

    Added function to recurring task

--- src/main/java/Task/RecurringTask.java
+++ src/main/java/Task/RecurringTask.java
@@ -4,4 +4,18 @@ public class RecurringTask extends Task{
     int EndDate;
     int StartDate;
     int Frequency;
+
+    public RecurringTask(){
+        StartDate = Date;
+    }
+
+    public boolean SetFrequency(int frequency){
+        if ((frequency == 1) || (frequency == 7)){
+            Frequency = frequency;
+
+            return true;
+        }
+
+        return false;
+    }
 }
