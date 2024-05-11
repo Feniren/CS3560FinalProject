5f9b3196754fce01466fc2de49210391f02ccaf6
Cat <catdavis25@gmail.com>
2024-05-10 18:05:13 -0700

    changed into maven for dependencies

---
+++ src/main/PSS.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+public class PSS {
+    /*public static void readToFile(){
+        try{
+
+        }
+        catch(){
+
+        }
+    }
+    public static void writeToFile(){
+        try{
+
+        }
+        catch(){
+
+        }
+    }
+    public static void accessSchedule(){
+
+    }*/
+}
