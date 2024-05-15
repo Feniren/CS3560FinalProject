3d189fb113d81e0a8430c7adcff736d450fd27b3
Feniren <sanders.aidan.c.ss@gmail.com>
2024-05-14 17:50:17 -0700

    Filled out Task Classes

--- src/main/java/Task/AntiTask.java
+++ src/main/java/Task/AntiTask.java
@@ -1,6 +1,35 @@
 package Task;
 
 public class AntiTask extends Task{
-    AntiTask(){
+    public AntiTask(){
+        TaskTypes = new String[1];
+
+        TaskTypes[0] = "Cancellation";
+    }
+
+    public AntiTask(int date, float duration, String name, float startTime, String type){
+        TaskTypes = new String[1];
+
+        TaskTypes[0] = "Cancellation";
+
+        if (!SetDate(date)){
+            SetDate(20240101);
+        }
+
+        if (!SetDuration(duration)){
+            SetDuration(0.25f);
+        }
+
+        if (!SetName(name)){
+            SetName(name);
+        }
+
+        if (!SetStartTime(startTime)){
+            SetStartTime(0.0f);
+        }
+
+        if (!SetType(type)){
+            SetType("Cancellation");
+        }
     }
 }
